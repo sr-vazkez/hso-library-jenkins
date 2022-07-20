95bb8bda67e81ac9629e19e666e20c8c74a14195
don-polo <paul.vazquez@harso.com>
2022-07-20 14:44:49 -0500

    Preparando libreria para jenkins

--- vars/jiraCreateIssueWindows.groovy
+++ vars/jiraCreateIssueWindows.groovy
@@ -9,5 +9,5 @@ def call(Map config=[:]) {
   def render = renderTemplate(rawBody,binding)
   // ahora tendra que executar un curl pero desde un powershell script
 
-  powershell('curl -D- -u $JIRA_CREDENTIALS -X POST --data "'+render+'" -H "Content-Type: application/json" $JIRA_URL/rest/api/2/issue')
+  powershell('curl.exe -D- -u $JIRA_CREDENTIALS -X POST --data "'+render+'" -H "Content-Type: application/json" $JIRA_URL/rest/api/2/issue')
 }
